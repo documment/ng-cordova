ba8e2dfc08b82337d877fc93da8061212f2de5df
surfacelevel <dev@surfacelevel.com>
2014-11-16 07:14:20 -0800

    $cordovaCamera mock and test: convert tabs to spaces

--- src/mocks/camera.js
+++ src/mocks/camera.js
@@ -5,34 +5,34 @@
  * @description
  * A service for testing camera features
  * in an app build with ngCordova.
-**/ 
+**/
 ngCordovaMocks.factory('$cordovaCamera', ['$q', function($q) {
-	var throwsError = false;
+    var throwsError = false;
 
-	return {
+    return {
         /**
-		 * @ngdoc property
-		 * @name throwsError
-		 * @propertyOf ngCordovaMocks.cordovaCamera
-		 *
-		 * @description
-		 * A flag that signals whether a promise should be rejected or not. 
-		 * This property should only be used in automated tests.
-		**/		
-		throwsError: throwsError,
+         * @ngdoc property
+         * @name throwsError
+         * @propertyOf ngCordovaMocks.cordovaCamera
+         *
+         * @description
+         * A flag that signals whether a promise should be rejected or not.
+         * This property should only be used in automated tests.
+        **/
+        throwsError: throwsError,
 
-		getPicture: function(options) {
-			var defer = $q.defer();
-			if (this.throwsError) {
-				defer.reject('There was an error getting the picture.');
-			} else {
-				if (options) {
-					options = options;	// This is just to get by JSHint.
-				}
+        getPicture: function(options) {
+            var defer = $q.defer();
+            if (this.throwsError) {
+                defer.reject('There was an error getting the picture.');
+            } else {
+                if (options) {
+                    options = options;	// This is just to get by JSHint.
+                }
 
-				defer.resolve();					
-			}
-			return defer.promise;
-		}
-	};
+                defer.resolve();
+            }
+            return defer.promise;
+        }
+    };
 }]);
