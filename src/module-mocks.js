966f2b913982be8791e2a0f5a998c6a757e27981
surfacelevel <dev@surfacelevel.com>
2014-11-16 14:13:08 -0800

    mock $cordovaCamera renamed to ngCordovaMocks.$cordovaCamera
    Currently each cordova plugin wrapper and corresponding mock are named
    the same. This means that both can't be included in a project at the
    same time because there is no namespacing of components from different
    modules. This forces a developer to have preprocessor step in their
    build pipeline and doesn't work well in scenarios when a developer is
    designing a responsive interface with new tools such as browsersync
    http://www.browsersync.io/ .
    
    The issue is also talked about here:
    
    http://stackoverflow.com/questions/15342657/how-to-reference-values-with-the-same-name-from-different-modules
    
    The suggestion given in the answer seems to be popular with many
    developers:
    
    > your best option is to prefix your values
    > (and other providers) with a module name
    
    This commit demonstrates what that suggestion looks like when
    implemented. Subsequent commits will show how the
    ngCordovaMocks.$cordovaCamera and other mocks can now easily be used
    without name collisions.

--- src/module-mocks.js
+++ src/module-mocks.js
@@ -1,1 +1,19 @@
 var ngCordovaMocks = angular.module('ngCordovaMocks', []);
+
+ngCordovaMocks.config(['$provide', Config]);
+
+function Config($provide) {
+    $provide.factory('$cordovaCamera', injectCordovaCamera);
+    $provide.factory('$cordovaCameraConstants', injectCordovaCameraConstants);
+
+    injectCordovaCamera.$inject = ['ngCordovaMocks.$cordovaCamera'];
+    injectCordovaCameraConstants.$inject = ['ngCordovaMocks.$cordovaCameraConstants'];
+
+    function injectCordovaCamera($cordovaCamera) {
+        return $cordovaCamera;
+    }
+
+    function injectCordovaCameraConstants($cordovaCameraConstants) {
+        return $cordovaCameraConstants;
+    }
+}
