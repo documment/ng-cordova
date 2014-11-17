07e753fa40b7d1f54bd8db994ce6cacb9f4c1d13
surfacelevel <dev@surfacelevel.com>
2014-11-16 17:18:35 -0800

    Adds $cordovaCameraConstants and ngCordovaMocks.$cordovaCameraConstants
    The `$cordovaCamera` plugin has a number of constants that are exposed
    on `window` if cordova.js and cordova_plugins.js are included. When not
    running on a device or in test and exception will be thrown if they are
    accessed. CameraConstants.js is where constants are defined. The mock
    contains a copy of the contents of CameraConstants.js.
    
    ngCordovaMocks.$cordovaCameraConstants is injected in the test as
    $cordovaCameraConstants. The same could be done in a `config()` of
    a module if the application is running outside of a device
    
    In the camera plugin test a `beforeEach()` is added the injects
    `$window.Camera` with `ngCordovaMocks.$cordovaCameraConstants` in
    case a test is added in the future that needs the value of one of
    the constants

--- test/mocks/camera.spec.js
+++ test/mocks/camera.spec.js
@@ -1,15 +1,29 @@
 describe('ngCordovaMocks', function() {
-    beforeEach(function() {
-        module('ngCordovaMocks');
-    });
+    beforeEach(module('ngCordovaMocks', function($provide) {
+        $provide.factory('$cordovaCamera', injectCordovaCamera);
+        $provide.factory('$cordovaCameraConstants', injectCordovaCameraConstants);
+
+        injectCordovaCamera.$inject = ['ngCordovaMocks.$cordovaCamera'];
+        injectCordovaCameraConstants.$inject = ['ngCordovaMocks.$cordovaCameraConstants'];
+
+        function injectCordovaCamera($cordovaCamera) {
+            return $cordovaCamera;
+        }
+
+        function injectCordovaCameraConstants($cordovaCameraConstants) {
+            return $cordovaCameraConstants;
+        }
+    }));
 
     describe('cordovaCamera', function () {
         var $rootScope = null;
         var $cordovaCamera = null;
+        var $cordovaCameraConstants = null;
         var cameraOptions = {};
 
-        beforeEach(inject(function (_$cordovaCamera_, _$rootScope_) {
+        beforeEach(inject(function (_$cordovaCamera_, _$cordovaCameraConstants_, _$rootScope_) {
             $cordovaCamera = _$cordovaCamera_;
+            $cordovaCameraConstants = _$cordovaCameraConstants_;
             $rootScope = _$rootScope_;
         }));
 
@@ -35,7 +49,49 @@ describe('ngCordovaMocks', function() {
                 .finally(function() { done(); })
             ;
 
+			$rootScope.$digest();
+		});
+
+        it('should get picture as data url', function(done) {
+            $cordovaCamera.dataUrl = true;
+            cameraOptions.destinationType = $cordovaCameraConstants.DestinationType.DATA_URL;
+            $cordovaCamera.getPicture(cameraOptions)
+                .then(
+                    function(pictureResult) { expect(pictureResult).toBe(true); },
+                    function() { expect(false).toBe(true); }
+                )
+                .finally(function() { done(); })
+            ;
+
+            $rootScope.$digest();
+        });
+
+        it('should get picture as a file uri', function (done) {
+            $cordovaCamera.fileUri = true;
+            cameraOptions.destinationType = $cordovaCameraConstants.DestinationType.FILE_URI;
+            $cordovaCamera.getPicture(cameraOptions)
+                .then(
+                    function(pictureResult) { expect(pictureResult).toBe(true); },
+                    function() { expect(false).toBe(true); }
+                )
+                .finally(function() { done(); })
+            ;
+
+            $rootScope.$digest();
+        });
+
+        it('should get picture as a native uri', function (done) {
+            $cordovaCamera.nativeUri = true;
+            cameraOptions.destinationType = $cordovaCameraConstants.DestinationType.NATIVE_URI;
+            $cordovaCamera.getPicture(cameraOptions)
+                .then(
+                    function(pictureResult) { expect(pictureResult).toBe(true); },
+                    function() { expect(false).toBe(true); }
+                )
+                .finally(function() { done(); })
+            ;
+
             $rootScope.$digest();
         });
     });
-})
+});
