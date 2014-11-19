8a9cd7b9a4de648a3102ebdbee9edb3ebb92bee5
surfacelevel <dev@surfacelevel.com>
2014-11-19 07:45:12 -0500

    Adds $cordovaScreenOrientation to query the current screen orientation
    The screen orientation is described in terms if iPhone device
    orientation. As described here:
    http://www.matthewgifford.com/blog/2011/12/22/a-misconception-about-window-orientation/
    the natural ortiation of a device is not standardized. However in VERY limited device
    testing all android devices exhibited the same orientation as the iPhone

---
+++ src/plugins/screenOrientation.js
@@ -0,0 +1,73 @@
+(function () {
+    'use strict';
+
+    // As described in Supported Interface Orientations: https://developer.apple.com/library/ios/technotes/tn2244/_index.html
+    var ScreenOrientationConstants = {
+        portrait: 'portrait',
+        upsideDown: 'upsideDown', // upside down portrait
+        landscapeRight: 'landscapeRight', // hold iPhone in right hand and turn counter clockwise
+        landscapeLeft: 'landscapeLeft', // hold iPhone in left hand and turn clockwise
+        unknown: 'unknown'
+    };
+
+    angular
+    .module('ngCordova.plugins.screenOrientation', [])
+    .constant('$cordovaScreenOrientationConstants', ScreenOrientationConstants)
+    .factory('$cordovaScreenOrientation', ScreenOrientation);
+
+    ScreenOrientation.$inject = ['$window', '$log'];
+
+    function ScreenOrientation($window, $log) {
+        return {
+            isPortrait: isPortrait,
+            isUpsideDown: isUpsideDown,
+            isLandscapeRight: isLandscapeRight,
+            isLandscapeLeft: isLandscapeLeft,
+            isUnknown: isUnknown
+        };
+
+        function isPortrait() {
+            return readOrientation() === ScreenOrientationConstants.portrait;
+        }
+
+        function isUpsideDown() {
+            return readOrientation() === ScreenOrientationConstants.upsideDown;
+        }
+
+        function isLandscapeRight() {
+            return readOrientation() === ScreenOrientationConstants.landscapeRight;
+        }
+
+        function isLandscapeLeft() {
+            return readOrientation() === ScreenOrientationConstants.landscapeLeft;
+        }
+
+        function isUnknown() {
+            return readOrientation() === undefined;
+        }
+
+        function readOrientation() {
+            var orientationName;
+            switch ($window.orientation) {
+                case 0:
+                    orientationName = ScreenOrientationConstants.portrait;
+                    break;
+                case 180:
+                    orientationName = ScreenOrientationConstants.upsideDown;
+                    break;
+                case -90:
+                    orientationName = ScreenOrientationConstants.landscapeRight;
+                    break;
+                case 90:
+                    orientationName = ScreenOrientationConstants.landscapeLeft;
+                    break;
+                default:
+                    orientationName = ScreenOrientationConstants.unknown;
+            }
+
+            $log.debug('Orientation is: ' + orientationName);
+            return orientationName;
+        }
+    }
+
+})();
