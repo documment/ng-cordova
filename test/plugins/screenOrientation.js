cde9da773375e072d5e1016e044f1db49f87a7da
surfacelevel <dev@surfacelevel.com>
2014-11-21 09:47:24 -0500

    $cordovaScreenOrientation: can now build a boxed orientation by name
    Given an orientation name from `$cordovaScreenOrientationConstants` it
    is helpful to be able to get boxed object version of the name like you
    already can for the current $window.orientation value. The
    `$cordovaScreenOrientation` has been slightly refactored to allow for this
    use case. It creates a new instance of boxed orientation each time the
    `$cordovaScreenOrientation.get([orientationName])` is called
    
    When the boxed orientation is a wrapper for `$window.orientation`
    care is taken not assign the value `$window.orientation` to a local
    variable. This ensures that the boxed orientation always gives the
    correct answer if cached and reused.

--- test/plugins/screenOrientation.js
+++ test/plugins/screenOrientation.js
@@ -6,7 +6,8 @@
         portrait: 'portrait',
         upsideDown: 'upsideDown', // upside down portrait
         landscapeRight: 'landscapeRight', // hold iPhone in right hand and turn counter clockwise
-        landscapeLeft: 'landscapeLeft' // hold iPhone in left hand and turn clockwise
+        landscapeLeft: 'landscapeLeft', // hold iPhone in left hand and turn clockwise
+        unknown: 'unknown'
     };
 
     angular
@@ -18,52 +19,84 @@
 
     function ScreenOrientation($window, $log) {
         return {
-            isPortrait: isPortrait,
-            isUpsideDown: isUpsideDown,
-            isLandscapeRight: isLandscapeRight,
-            isLandscapeLeft: isLandscapeLeft,
-            isUnknown: isUnknown
+            get: readOrientation
         };
 
-        function isPortrait() {
-            return readOrientation() === ScreenOrientationConstants.portrait;
-        }
+        function readOrientation(orientation) {
+            return Object.create(OrientationBox.prototype, {
+                orientation: {
+                    get: function() {
+                        return getOrientationFn();
+                    }
+                }
+            });
 
-        function isUpsideDown() {
-            return readOrientation() === ScreenOrientationConstants.upsideDown;
-        }
-
-        function isLandscapeRight() {
-            return readOrientation() === ScreenOrientationConstants.landscapeRight;
-        }
-
-        function isLandscapeLeft() {
-            return readOrientation() === ScreenOrientationConstants.landscapeLeft;
-        }
-
-        function isUnknown() {
-            return readOrientation() === undefined;
-        }
-
-        function readOrientation() {
-            var orientationName;
-            switch ($window.orientation) {
-                case 0:
-                    orientationName = ScreenOrientationConstants.portrait;
-                    break;
-                case 180:
-                    orientationName = ScreenOrientationConstants.upsideDown;
-                    break;
-                case -90:
-                    orientationName = ScreenOrientationConstants.landscapeRight;
-                    break;
-                case 90:
-                    orientationName = ScreenOrientationConstants.landscapeLeft;
-                    break;
+            function getOrientationFn() {
+                if(orientation)
+                    return getOrientationFromArg;
+                else
+                    return getOrientationFromWindow;
             }
 
-            $log.debug('Orientation is: ' + orientationName);
-            return orientationName;
+            function getOrientationFromArg() {
+                return orientation;
+            }
+
+            function getOrientationFromWindow() {
+                var orientationName;
+                switch ($window.orientation) {
+                    case 0:
+                        orientationName = ScreenOrientationConstants.portrait;
+                        break;
+                    case 180:
+                        orientationName = ScreenOrientationConstants.upsideDown;
+                        break;
+                    case -90:
+                        orientationName = ScreenOrientationConstants.landscapeRight;
+                        break;
+                    case 90:
+                        orientationName = ScreenOrientationConstants.landscapeLeft;
+                        break;
+                }
+
+                $log.debug('Orientation is: ' + orientationName);
+                return orientationName;
+            }
+        }
+    }
+
+    function OrientationBox() {}
+
+    OrientationBox.prototype = {
+        getName: function () {
+            var orientation = this.orientation();
+            if(!orientation)
+                orientation = ScreenOrientationConstants.unknown;
+            return orientation;
+        },
+
+        isPortrait: function () {
+            return this.getName() === ScreenOrientationConstants.portrait;
+        },
+
+        isUpsideDown: function () {
+            return this.getName() === ScreenOrientationConstants.upsideDown;
+        },
+
+        isLandscapeRight: function() {
+            return this.getName() === ScreenOrientationConstants.landscapeRight;
+        },
+
+        isLandscapeLeft: function() {
+            return this.getName() === ScreenOrientationConstants.landscapeLeft;
+        },
+
+        isUnknown: function () {
+            return this.getName() === ScreenOrientationConstants.unknown;
+        },
+
+        toString: function () {
+            return this.getName();
         }
     }
 
--- test/plugins/screenOrientation.spec.js
+++ test/plugins/screenOrientation.spec.js
@@ -2,42 +2,71 @@ describe('Service: $cordovaScreenOrientation\n', function () {
 
     beforeEach(module('ngCordova.plugins.screenOrientation'));
 
-    describe('Detecting current screen orientation', function () {
-        var $window,
-            $cordovaScreenOrientation,
-            $cordovaScreenOrientationConstant;
+    var $window,
+    $cordovaScreenOrientation,
+    $cordovaScreenOrientationConstant;
 
-        beforeEach(inject(function (_$window_, _$cordovaScreenOrientation_, _$cordovaScreenOrientationConstants_) {
-            $window = _$window_;
-            $cordovaScreenOrientation = _$cordovaScreenOrientation_;
-            $cordovaScreenOrientationConstant = _$cordovaScreenOrientationConstants_;
-        }));
+    beforeEach(inject(function (_$window_, _$cordovaScreenOrientation_, _$cordovaScreenOrientationConstants_) {
+        $window = _$window_;
+        $cordovaScreenOrientation = _$cordovaScreenOrientation_;
+        $cordovaScreenOrientationConstant = _$cordovaScreenOrientationConstants_;
+    }));
+
+    describe('Detecting current screen orientation', function () {
 
         it('should detect the screen in portrait', function (done) {
             $window.orientation = 0;
-            expect($cordovaScreenOrientation.isPortrait()).toBe(true);
+            expect($cordovaScreenOrientation.get().isPortrait()).toBe(true);
         });
 
         it('should detect the screen is upside down', function (done) {
             $window.orientation = 180;
-            expect($cordovaScreenOrientation.isUpsideDown()).toBe(true);
+            expect($cordovaScreenOrientation.get().isUpsideDown()).toBe(true);
         });
 
         it('should detect the screen is landscape right', function (done) {
             $window.orientation = -90;
-            expect($cordovaScreenOrientation.isLandscapeRight()).toBe(true);
+            expect($cordovaScreenOrientation.get().isLandscapeRight()).toBe(true);
         });
 
         it('should detect the screen is landscape left', function (done) {
             $window.orientation = 90;
-            expect($cordovaScreenOrientation.isLandscapeLeft()).toBe(true);
+            expect($cordovaScreenOrientation.get().isLandscapeLeft()).toBe(true);
         });
 
         it('should report if the orientation is unknown', function (done) {
             $window.orientation = null;
-            expect($cordovaScreenOrientation.isUnknown()).toBe(true);
+            expect($cordovaScreenOrientation.get().isUnknown()).toBe(true);
         });
 
+        it('should allow the orientation name to be queried', function (done) {
+            $window.orientation = 0;
+            expect($cordovaScreenOrientation.get().getName())
+            .toBe($cordovaScreenOrientationConstant.portrait);
+        });
+
+        it('should not cache the $window.orientation value', function (done) {
+            $window.orientation = 0;
+            var orientation = $cordovaScreenOrientation.get();
+            expect(orientation.isPortrait()).toBe(true);
+
+            $window.orientation = 180;
+            expect(orientation.isUpsideDown()).toBe(true);
+        });
     });
 
+    describe('Usages of orientation name', function() {
+
+        it('should build an orientation object from an orientation name', function (done) {
+            expect($cordovaScreenOrientation.get(
+                $cordovaScreenOrientationConstant.portrait
+            ).isPortrait()).toBe(true);
+        });
+
+        it('should build an orientation object from an orientation name', function (done) {
+            expect($cordovaScreenOrientation.get(
+            $cordovaScreenOrientationConstant.portrait
+            ).getName()).toBe($cordovaScreenOrientationConstant.portrait);
+        });
+    })
 });
